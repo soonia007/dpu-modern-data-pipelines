e9f8ff946a5e26c61b7a28a0ef0b8654e3a14e81
Kantapon Nile <111864038+soonia007@users.noreply.github.com>
2025-02-14 09:55:50 +0000

    update W2-W3

---
+++ dags/weather_api_dag.py
@@ -0,0 +1,111 @@
+import json
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils import timezone
+
+import requests
+
+
+DAG_FOLDER = "/opt/airflow/dags"
+
+
+def _get_weather_data():
+    # API_KEY = os.environ.get("WEATHER_API_KEY")
+    API_KEY = Variable.get("weather_api_key")
+
+    payload = {
+        "q": "bangkok",
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    response = requests.get(url, params=payload)
+    print(response.url)
+
+    data = response.json()
+    print(data)
+
+    with open(f"{DAG_FOLDER}/data.json", "w") as f:
+        json.dump(data, f)
+
+        
+
+def _validate_data():
+    with open("/opt/airflow/dags/data.json", "r") as f:
+        data = json.load(f)
+
+    assert data.get("main") is not None
+
+def _create_weather_table():
+    pg_hook = PostgresHook(
+        postgres_conn_id="weather_postgres_conn",
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    sql = """
+        CREATE TABLE IF NOT EXISTS weathers (
+            dt BIGINT NOT NULL,
+            temp FLOAT NOT NULL
+        )
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+def _load_data_to_postgres():
+    pg_hook = PostgresHook(
+        postgres_conn_id="weather_postgres_conn",
+        schema="postgres"
+    )
+    connection = pg_hook.get_conn()
+    cursor = connection.cursor()
+
+    with open("/opt/airflow/dags/data.json", "r") as f:
+        data = json.load(f)
+
+    temp = data["main"]["temp"]
+    dt = data["dt"]
+    sql = f"""
+        INSERT INTO weathers (dt, temp) VALUES ({dt}, {temp})
+    """
+    cursor.execute(sql)
+    connection.commit()
+
+
+with DAG(
+    "weather_api_dag",
+    schedule="0 */3 * * *",
+    start_date=timezone.datetime(2025, 2, 1),
+    tags=["dpu"],
+):
+    start = EmptyOperator(task_id="start")
+
+    get_weather_data = PythonOperator(
+        task_id="get_weather_data",
+        python_callable=_get_weather_data,
+    )
+
+    validate_data = PythonOperator(
+        task_id="validate_data",
+        python_callable=_validate_data,
+    )
+
+    create_weather_table = PythonOperator(
+        task_id="create_weather_table",
+        python_callable=_create_weather_table,
+    )
+
+    load_data_to_postgres = PythonOperator(
+        task_id="load_data_to_postgres",
+        python_callable=_load_data_to_postgres,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> get_weather_data >> validate_data >> create_weather_table >> load_data_to_postgres >> end
--- extract.py
+++ extract.py
@@ -15,3 +15,35 @@ if __name__ == "__main__":
     # Write data to file
     with open("dogs.json", "w") as f:
         json.dump(data, f)
+
+
+
+
+if __name__ == "__main__":
+    # Get data from Dog API
+    # โค้ดส่วนนี้จะเป็นการเชื่อมต่อกับ Dog API โดยเราจะใส่ URL Endpoint
+    url = "https://dog.ceo/api/breeds/image/random"
+    # เสร็จแล้วจะยิง Request ไปดึงข้อมูลมาเก็บใส่ Response
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    # Write data to file
+    with open("dogs.json", "w") as f:
+        json.dump(data, f)
+
+    # โค้ดส่วนนี้จะเป็นการเตรียมเรื่อง Credentials และ Configuration ต่าง ๆ เพื่อใช้ในการเชื่อมต่อกับ Dog API
+    api_url = "https://api.jsonbin.io/v3/b"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Master-Key": "$2a$10$rO3m/MnafmnrgC0kv8/lm.VlY5tClu7Q6ikL7UPo0z9g1nJuikqyK",
+        "X-Collection-Id": "678b6261acd3cb34a8ce4087",
+    }
+
+    # Read data from file
+    with open("dogs.json", "r") as f:
+        data = json.load(f)
+
+    # โค้ดส่วนนี้จะเป็นการยิง Request ไปเพื่ออัพโหลดข้อมูลไปยังเซิฟเวอร์ของ JSONBin
+    response = requests.post(api_url, json=data, headers=headers)
+    print(response.json())
